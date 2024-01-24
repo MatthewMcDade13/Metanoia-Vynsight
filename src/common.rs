b812efaceb0e6cde0b91c40ca809f2e4fd749c1b
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2024-01-23 17:23:20 -0800

    begin total refactor

--- src/common.rs
+++ src/common.rs
@@ -1,87 +1,80 @@
-use std::io::Cursor;
+use std::sync::mpsc::{channel, Receiver, Sender};
 
-use actix::{Addr, Message};
-use serde::{Serialize, Deserialize};
-use crate::crawl::{Crawler};
-use crate::error::spider::CrawlError;
-use crate::web::Target;
-use crate::spider_sup::{SpiderStatus};
+use anyhow::bail;
 
 #[macro_export]
 macro_rules! json_from_file {
     ($output_type:ty, $filepath:ident) => {{
-        use crate::common::{DynResult, ser::from_json};
-            (|| -> DynResult<$output_type> {
-                 let json = std::fs::read_to_string($filepath)?;
-                 let value = from_json::<$output_type>(&json)?;
-                 Ok(value)
-             })()
+        (|| -> anyhow::Result<$output_type> {
+            let json = std::fs::read_to_string($filepath)?;
+            let value = from_json::<$output_type>(&json)?;
+            Ok(value)
+        })()
     }};
     ($output_type:ty, $filepath:literal) => {{
-        use crate::common::{DynResult, from_json};
-            (|| -> DynResult<$output_type> {
-                 let json = std::fs::read_to_string($filepath)?;
-                 let value = from_json::<$output_type>(&json)?;
-                 Ok(value)
-             })()
+        (|| -> anyhow::Result<$output_type> {
+            let json = std::fs::read_to_string($filepath)?;
+            let value = from_json::<$output_type>(&json)?;
+            Ok(value)
+        })()
     }};
 }
 
-pub type HyperClient = hyper::client::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>;
-
-
-
-#[derive(Debug, Message)]
-#[rtype(result = "()")]
-pub struct Kill;
-
-#[derive(Debug, Message, Clone)]
-#[rtype(result = "()")]
-pub struct DoneCrawl {
-    pub result: Result<Target, CrawlError>,
-    pub sender: Addr<Crawler>
-}
-
-#[derive(Debug, Clone)]
-pub struct CrawlResult(pub hyper::http::Uri, pub Target);
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct SpawnCrawler;
-
-type TargetUri = String;
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct Crawl(pub TargetUri);
-
-#[derive(Debug, Message)]
-#[rtype(result = "()")]
-pub struct SpiderDone(pub Vec<CrawlResult>);
-
-
-#[derive(Debug, Message)]
-#[rtype(result = "SpiderStatus")]
-pub struct GetSpiderStatus;
-
-pub type DynResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
-#[allow(dead_code)]
 pub mod ser {
-    use super::*;
+    use serde::{Deserialize, Serialize};
 
-    pub fn into_json<'a, T: 'a>(value: &T)  -> DynResult<String>
-        where T: Serialize + Deserialize<'a> {
-            
-            let json = serde_json::ser::to_string_pretty(value)?;
-    
-            Ok(json)
+    pub fn into_json<T>(value: &T) -> anyhow::Result<String>
+    where
+        for<'a> T: Serialize + Deserialize<'a>,
+    {
+        let json = serde_json::ser::to_string_pretty(value)?;
+
+        Ok(json)
     }
-    
-    pub fn from_json<T>(json: &str) -> DynResult<T> 
-        where for<'a> T: Deserialize<'a> {
+
+    pub fn from_json<T>(json: &str) -> anyhow::Result<T>
+    where
+        for<'a> T: Deserialize<'a>,
+    {
         let value = serde_json::de::from_str::<T>(&json)?;
-        
+
         Ok(value)
     }
+}
 
-}    
+pub struct Channel<T> {
+    sender: Sender<T>,
+    recvr: Receiver<T>,
+}
 
+impl<T> Channel<T> {
+    pub fn new() -> Self {
+        let (sender, recvr) = channel::<T>();
+        Self { sender, recvr }
+    }
+
+    pub fn send(&self, v: T) {
+        // We know sender is alive as long as this Channel object has not been dropped
+        // so this should not panic on unwrap
+        self.sender.send(v).unwrap()
+    }
+
+    pub fn recv(&self) -> T {
+        self.recvr.recv().unwrap()
+    }
+
+    pub fn try_send(&self, v: T) -> anyhow::Result<()> {
+        if let Err(_) = self.sender.send(v) {
+            bail!("Error sending to channel")
+        }
+        Ok(())
+    }
+
+    pub fn try_recv(&self) -> anyhow::Result<T> {
+        self.recvr.try_recv().map_err(|e| anyhow::anyhow!(e))
+    }
+
+    pub fn clone_sender(&self) -> Sender<T> {
+        self.sender.clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,24 @@
-extern crate lmdb;
-extern crate hyper;
-extern crate hyper_tls;
-extern crate actix;
-extern crate ciborium;
-extern crate scraper;
-extern crate clap;
-extern crate serde_json;
+use clap::{arg, command};
 
-
-use common::{ser::{into_json, from_json}, CrawlResult};
-use error::spider::SpiderFailure;
-use hyper::{Client, Uri};
-use hyper_tls::HttpsConnector;
-use serde::{Serialize, Deserialize};
-use spider_sup::{SpiderStatus};
-use std::error::Error;
-use clap::{command, arg};
-use actix::prelude::*;
-
-use crate::spider::{Spider, SpiderConfig};
-
-
-mod db;
-mod spider;
-mod spider_sup;
-mod crawl;
-mod scrape;
 mod common;
-mod error;
-mod web;
-
-type MainResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 const DEFAULT_CONFIG_PATH: &'static str = "./src/pub/config.json";
 
-#[actix::main]
-async fn main() -> MainResult<()> {
-
+fn main() -> anyhow::Result<()> {
     let args = command!()
-        .arg(arg!(--config <PATH>).required(false).default_value(DEFAULT_CONFIG_PATH))
+        .arg(
+            arg!(--config <PATH>)
+                .required(false)
+                .default_value(DEFAULT_CONFIG_PATH),
+        )
         .get_matches();
-        // .arg(arg!(--blind).required_unless_present("config"))
-        // .arg(arg!(--search <TERM>).required_unless_present_any(["config", "blind"]))
-        // .arg(arg!(--targets <FILEPATH>).required_unless_present_any(["config", "db"]))
-        // .arg(arg!(--db).required_unless_present_any(["targets", "config"]))
-        // .arg(arg!(--single).required_unless_present("config"))
-        // .get_matches();
+    // .arg(arg!(--blind).required_unless_present("config"))
+    // .arg(arg!(--search <TERM>).required_unless_present_any(["config", "blind"]))
+    // .arg(arg!(--targets <FILEPATH>).required_unless_present_any(["config", "db"]))
+    // .arg(arg!(--db).required_unless_present_any(["targets", "config"]))
+    // .arg(arg!(--single).required_unless_present("config"))
+    // .get_matches();
 
-    if let Some(cfg_path) = args.get_one::<String>("config") {
-
-        let config = get_config(cfg_path)?;
-
-        let spider_result = run_spider(&config).await?;
-
-        exit_on_ok(&spider_result)
-    } else {
-        // Bail, we dont have any input
-        exit_on_err(&SpiderFailure("No input provided for Crawling".into()))
-    }
-
-
-   
-    
-}
-
-fn get_config(cfg_path: &str) -> MainResult<SpiderConfig> {
-    let mut config = json_from_file!(SpiderConfig, cfg_path)?;
-    let tfopt = config.target_file.clone();
-    if let Some(targets_file) = tfopt.as_deref() {
-        let _ = config.populate_targets_from_file(targets_file)?;
-    }
-    Ok(config)
-}
-
-async fn run_spider(config: &SpiderConfig) -> MainResult<Vec<CrawlResult>> {
-    let targets = config.targets.uris().to_vec();
-    let spider = Spider::new(&targets);
-    let spider_result = spider.run().await?;    
-    
-    Ok(spider_result)
-}
-
-fn exit_on_ok(new_targets: &Vec<CrawlResult>) -> MainResult<()> {
-    // TODO :: Save new_targets to DB / Text file for next run.
-    println!("Success! new_targets = {:?}", new_targets);
     Ok(())
 }
 
-fn exit_on_err(err: &SpiderFailure) -> MainResult<()> {
-    let b = Box::new(err.clone()); 
-    Err(b)
-}
